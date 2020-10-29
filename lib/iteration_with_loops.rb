2c042761cba4afdc4a06eed0c76f8173610f2553
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-29 22:41:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,12 +2,16 @@ def find_min_in_nested_arrays(src)
   
   outer_results = []
   rows = 0
+  
   while rows < src.count do
     elements = 0
     low_daily_temps = 100
+    
     while elements < src[rows].count do
+      
       if src[rows][elements] < low_daily_temps
         low_daily_temps = src[rows][elements]
+        
       end
       elements += 1
     end
